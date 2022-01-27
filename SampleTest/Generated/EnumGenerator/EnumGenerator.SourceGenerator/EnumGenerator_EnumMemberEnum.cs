85c526320e4464399372bc91ce01762a673beba8
Pascal Nsunba <PascalBenstrong@users.noreply.github.com>
2022-01-27 21:23:09 +0200

    removed generated source generator files

---
+++ SampleTest/Generated/EnumGenerator/EnumGenerator.SourceGenerator/EnumGenerator_EnumMemberEnum.cs
@@ -0,0 +1,72 @@
+﻿
+using System;
+using SampleTest;
+
+namespace SampleTest.Generated.EnumExtensions
+{
+    public static partial class EnumExtensions
+    {
+        public static string GetString(this EnumMemberEnum @enum)
+        {
+            return @enum switch {
+
+            		EnumMemberEnum.Monday => "monday",
+					EnumMemberEnum.Tuesday => "Tuesday",
+					EnumMemberEnum.Wednesday => "Wednesday",
+					EnumMemberEnum.Thursday => "Thursday",
+					EnumMemberEnum.Friday => "Friday",
+					EnumMemberEnum.Saturday => "Saturday",
+					EnumMemberEnum.Sunday => "Sunday",
+					 _ => throw new ArgumentOutOfRangeException("Invalid argument")
+
+            };
+        }
+
+		public static EnumMemberEnum FromStringEnumMemberEnum(this string @string)
+        {
+            return @string switch {
+            		"Monday" => EnumMemberEnum.Monday,
+					"Tuesday" => EnumMemberEnum.Tuesday,
+					"Wednesday" => EnumMemberEnum.Wednesday,
+					"Thursday" => EnumMemberEnum.Thursday,
+					"Friday" => EnumMemberEnum.Friday,
+					"Saturday" => EnumMemberEnum.Saturday,
+					"Sunday" => EnumMemberEnum.Sunday,
+					 _ => throw new ArgumentOutOfRangeException("Invalid argument")
+
+            };
+
+        }
+
+		public static string GetString(this JsonPropertyEnum @enum)
+        {
+            return @enum switch {
+
+            		JsonPropertyEnum.One => "One",
+					JsonPropertyEnum.Two => "Two",
+					JsonPropertyEnum.Three => "Three",
+					JsonPropertyEnum.Four => "Four",
+					JsonPropertyEnum.Five => "Five",
+					 _ => throw new ArgumentOutOfRangeException("Invalid argument")
+
+            };
+
+        }
+
+		public static JsonPropertyEnum FromStringJsonPropertyEnum(this string @string)
+        {
+            return @string switch {
+            		"One" => JsonPropertyEnum.One,
+					"Two" => JsonPropertyEnum.Two,
+					"Three" => JsonPropertyEnum.Three,
+					"Four" => JsonPropertyEnum.Four,
+					"Five" => JsonPropertyEnum.Five,
+					 _ => throw new ArgumentOutOfRangeException("Invalid argument")
+
+            };
+
+        }
+
+
+    }
+}
